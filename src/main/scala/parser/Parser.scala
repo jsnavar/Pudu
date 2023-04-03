c0d0673e878bfcf77b6d00b64e9632436a91b265
Juan Navarro <juansnn@pm.me>
2023-04-03 22:02:54 +0000

    Simplified parser generation classes

--- src/main/scala/parser/Parser.scala
+++
@@ -1,5 +0,0 @@
-package pudu.parser
-
-/** Base class for parsers */
-abstract class Parser[Tree, Token <: scala.reflect.Enum](spec: ParserSpec[Tree, Token]):
-  def parse(tokens: Iterator[Token]): Tree
--- src/main/scala/parser/ParserSpec.scala
+++
@@ -1,5 +0,0 @@
-package pudu.parser
-
-/** Data needed to construct a Parser. It is serializable, which could be used to
- *  save the specification to a file */
-abstract class ParserSpec[Tree, Token <: scala.reflect.Enum] extends Serializable
