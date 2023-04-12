a2cbf9e28f7762a8f40512c97db06977c6bb3ed4
Juan Navarro <juansnn@pm.me>
2023-04-12 20:40:13 +0000

    Minor style changes

--- src/main/scala/parser/generator/SLRParserGenerator.scala
+++ src/main/scala/parser/generator/SLRParserGenerator.scala
@@ -13,30 +13,47 @@ class SLRParserGenerator[Tree, Token <: scala.reflect.Enum](lang: LanguageSpec[T
   // ((stateIndex, tokenOrdinal), Action)
   type ActionTableEntry = ((Int, Int), SRAction)
 
+  def shiftReduceResolution(rule: RuleT, terminal: Terminal[Any]): SRAction = ???
+
+  def actionTableKey(from: State, terminal: Terminal[Any]) =
+    val fromIdx = indexedStates(from)
+    val terminalOrdinal = terminal.ordinal
+    (fromIdx, terminalOrdinal)
+
+  def shiftTo(from: State, terminal: Terminal[Any], to: State): ActionTableEntry =
+    val key = actionTableKey(from, terminal)
+    val toIdx = indexedStates(to)
+    (key, Shift(toIdx))
+
+  def reduceBy(from: State, terminal: Terminal[Any], rule: RuleT): ActionTableEntry =
+    val key = actionTableKey(from, terminal)
+    (key, Reduce(rule))
+
   def edgeAction(from: (State, Terminal[Any]), to: State): ActionTableEntry =
     val (state, terminal) = from
     // if [A -> \alpha\cdot] is in state, and terminal is in Follow(A), then
     // reduce by the rule A -> \alpha
-    val reduceBy = state.filter((item: ItemT) => item.after.isEmpty
+    val reduceItems = state.filter((item: ItemT) => item.after.isEmpty
                                 && item.left != startSymbol
                                 && follow(item.left).contains(terminal))
-    if reduceBy.size > 1 then throw ReduceReduceConflictException()
+    if reduceItems.size > 1 then throw ReduceReduceConflictException()
     // if [A -> \alpha\cdot a\beta] is in state, and a = terminal, then
-    // shift to to
+    // shift to 'to'
     val shift = state.find(_.after.head == terminal).isDefined
-    if shift && reduceBy.size != 0 then
+    if shift && reduceItems.size != 0 then
       throw ShiftReduceConflictException()
-    else if shift then
-      ((indexedStates(state), terminal.ordinal), Shift(indexedStates(to)))
-    else
-      ((indexedStates(state), terminal.ordinal), Reduce(reduceBy.head.rule))
-
+    else if shift then shiftTo(state, terminal, to)
+    else reduceBy(state, terminal, reduceItems.head.rule)
 
   // ((stateIndex, nonTerminalSymbol), stateIndex)
   type GotoTableEntry = ((Int, Symbol), Int)
 
   def edgeGoto(from: (State, Symbol), to: State): GotoTableEntry =
-    ((indexedStates(from._1), from._2), indexedStates(to))
+    val fromState = from._1
+    val fromStateIdx = indexedStates(fromState)
+    val symbol = from._2
+    val toIdx = indexedStates(to)
+    (fromStateIdx, symbol) -> toIdx
 
   def parser: Iterator[Token] => Either[ErrorMsg, Tree] =
     // split automaton edges in terminal and nonterminal edges
@@ -44,16 +61,20 @@ class SLRParserGenerator[Tree, Token <: scala.reflect.Enum](lang: LanguageSpec[T
     val nonTerminalEdges = splittedEdges(false)
 
     val terminalEdges = splittedEdges(true)
-    // 'cast' symbols to 'Terminal[Any]'.
-    val castedTerminalEdges = terminalEdges.map((key, value) => key._2 match
-                                                  case terminal: Terminal[_] => ((key._1, terminal), value)
-                                                  case _ => throw IllegalArgumentException("There was a non terminal in 'terminal'"))
+    // cast symbols to 'Terminal[Any]'.
+    val castedTerminalEdges = terminalEdges.map((key, value) =>
+      val (keyState, keySymbol) = key
+      if keySymbol.isInstanceOf[Terminal[Any]] then
+        ((keyState, keySymbol.asInstanceOf[Terminal[Any]]), value)
+      else
+        throw Exception("There was a non terminal in 'terminal'"))
+
     // accept state:
     val startState = closure(Set(augmentedRule.toItem))
     val acceptStateIndex = indexedStates(lr0Automaton(startState, lang.start))
 
     // build action and goto tables, using the functions defined above, and
-    // add acceptance entry to actionTable
+    // add acceptance condition to actionTable
     val actionTable = castedTerminalEdges.map(edgeAction).updated((acceptStateIndex, eof.ordinal), Accept)
     val gotoTable = nonTerminalEdges.map(edgeGoto)
     lrParse(actionTable, gotoTable)
---
+++ src/test/scala/parser/generator/SLRParserGeneratorTest.scala
@@ -0,0 +1,11 @@
+import pudu.grammar._
+import pudu.parser._
+import pudu.parser.generator._
+
+
+class SLRParserGeneratorTest extends munit.FunSuite {
+
+
+
+
+}
