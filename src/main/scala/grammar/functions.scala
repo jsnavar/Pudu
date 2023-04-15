e61679f55e64d653f5f5db5e5a2e2e22dfca4619
Juan Navarro <juansnn@pm.me>
2023-04-15 05:52:04 +0000

    Get terminal names from enum

--- src/main/scala/grammar/functions.scala
+++ src/main/scala/grammar/functions.scala
@@ -3,7 +3,7 @@ package pudu.grammar
 /** Transforms a curried function 'f' of type T1 => T2 => ... => Tn => R into
  *  function calls equivalent to (omitting the asInstanceOf for readability):
  {{{
-   (args: Seq[ST]) => f(args(0))(args(1))(args(2))...
+   (args: Seq[ST]) => f(args(n-1))(args(n-2))(args(n-3))...
  }}}
  * In the future, the return type should be simplified to Seq[Any] => R, ommiting
  * the parameter ST. This should be possible using a recursive match type similar
@@ -24,22 +24,25 @@ inline def seq[ST, S, R](inline numPars: Int, inline depth: Int, inline fn: S =>
         case nextFn: Function1[_,_] =>
           seq(numPars, depth-1, nextFn)(args).asInstanceOf[ST]
 
-/** Gets the ordinal value of an enum case from it's type.
+/** Gets the ordinal value, and name of an enum case from its type.
  *  This macro abuses the implementation of Enums, using that
- *  when cases are parametrized, 'ordinal' is implemented as
+ *  when cases are parameterized, 'ordinal' is implemented as
  *  def ordinal = IntConstant //Actually a Literal with an IntConstant */
 import scala.quoted.*
-inline def enumOrdinal[T] = ${ enumOrdinalImpl[T] }
+inline def enumMetadata[T] = ${ enumMetadataImpl[T] }
 
-def enumOrdinalImpl[T: Type](using Quotes) =
+def enumMetadataImpl[T: Type](using Quotes) =
   import quotes.reflect.*
   val ts = TypeRepr.of[T].typeSymbol
+  val name = Expr(ts.name)
   val ordinalSymbol = ts.methodMember("ordinal").head
   // this requires the scalac option -Yretain-trees
   val rhs = ordinalSymbol.tree.asInstanceOf[DefDef].rhs
-  rhs match
+  val ordinal = rhs match
     case None => throw Exception("Tree not found. Please compile with option -Yretain-trees")
     case Some(x) => x.asExprOf[Int]
+  Expr.ofTuple(ordinal, name)
+
 
 inline def untupled[T1, T2, R] = Function.untupled[T1, T2, R]
 inline def untupled[T1, T2, T3, R] = Function.untupled[T1, T2, T3, R]
