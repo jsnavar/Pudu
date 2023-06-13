afd58f8c634503c324ad889e65905de28682c5d2
Juan Navarro <juansnn@pm.me>
2023-06-13 01:41:47 +0000

    make toSeqFn a macro

--- src/main/scala/pudu/grammar/functions.scala
+++ src/main/scala/pudu/grammar/functions.scala
@@ -1,18 +1,30 @@
 package pudu.grammar
 
-/* Given a function fn: Tup => ST, where Size[Tup] is equal to size, it returns a function Seq[ST] => ST,
- * which takes the first 'size' elements of the sequence, reverse them, cast them to a tuple Tup (!!),
- * and finally calls fn on that tuple.
+import scala.quoted.*
+
+/* Given a function fn: Tup => ST, where Tup is a tuple type of arity 'size',
+ * toSeqFnTuple(fn) is inlined to the function:
+ * {{{
+     (x: Seq[Args]) => fn((x(size - 1), x(size - 2), ..., x(1), x(0)).asInstanceOf[Tup])
+ * }}},
+ * that corresponds to calling 'fn' on the top 'size' elements of the stack, casted to Tup (!!) in
+ * reverse order (order is reversed because it is an stack).
  * By itself this is obviously unsafe, but used in reductions of a 'correctly implemented' shift
  * reduce parser, where the semantic actions were defined using the ::= extension method, then it is safe.
  * This can be proved using that actions in ::= are type checked: each argument match the SymData of the
  * corresponding symbol, and the return type matches the SymData of the NonTerminal in the lhs; and when the
  * parser reduces by a production, then the top of the stack has elements corresponding to the rhs of that production. */
-inline def toSeqFn[ArgsType, RetType, Tup <: Tuple](inline size: Int, inline fn: Tup => RetType): Seq[ArgsType] => RetType =
-  (args: Seq[ArgsType]) =>
-    val top = args.take(size).reverse.toArray[Any]
-    val fnArgs = Tuple.fromArray(top).asInstanceOf[Tup]
-    fn(fnArgs)
+inline def toSeqFnTuple[Args, Ret, Tup <: Tuple](inline fn: Tup => Ret): Seq[Args] => Ret =
+  ${ toSeqFnTupleImpl('{compiletime.constValue[Tuple.Size[Tup]]}, 'fn) }
+
+def toSeqFnTupleImpl[Args: Type, Ret: Type, Tup <: Tuple: Type](size: Expr[Int], fn: Expr[Tup => Ret])(using Quotes) =
+  val idxSeq = (0 until size.valueOrAbort).reverse.map(Expr.apply)
+  '{ (x: Seq[Args]) =>
+    ${
+      val argsExp = Expr.ofTupleFromSeq(idxSeq.map(i => '{x($i)}))
+      '{ $fn(${argsExp}.asInstanceOf[Tup]) }
+    }
+  }
 
 /* special case for unit productions */
 inline def toSeqFn[ArgsType, RetType, T](inline fn: T => RetType): Seq[ArgsType] => RetType =
@@ -23,7 +35,6 @@ inline def toSeqFn[ArgsType, RetType, T](inline fn: T => RetType): Seq[ArgsType]
  *  This macro abuses the implementation of Enums, using that
  *  when cases are parameterized, 'ordinal' is implemented as
  *  def ordinal = IntConstant //Actually a Literal with an IntConstant */
-import scala.quoted.*
 inline def enumMetadata[T] = ${ enumMetadataImpl[T] }
 
 def enumMetadataImpl[T: Type](using Quotes) =
