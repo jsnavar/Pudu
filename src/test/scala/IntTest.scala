4d4281242b4b7e05fd12c630332ef010bd63e0fb
Juan Navarro <juansnn@pm.me>
2023-06-25 05:03:31 +0000

    add more tests to IntTest

--- src/test/scala/IntTest.scala
+++ src/test/scala/IntTest.scala
@@ -3,41 +3,61 @@ import pudu.grammar._
 import pudu.parser._
 import pudu.parser.generator._
 
+/** Tests for the interaction of lexer and parser (something like integration testing,
+ * but not really).
+ * As this interface is just an Iterator[Token], there is not much to test here, so we
+ * just do some sanity checks. */
 class IntTest extends munit.FunSuite {
   val parser = LR1ParserGenerator(SimpleArithmetic.grammar).parser
+  val strParser = ArithmeticLexer.lexer.andThen(parser)
 
   test("eval 1") {
-    val input = "2 + 3 * pow(3, 5)"
-    val lexer = ArithmeticLexer.lexer(input)
-    val result = parser(lexer)
+    val result = strParser("2 + 3 * pow(3, 5)")
 
-    assert(result.isRight)
-    assertEquals(result.getOrElse(0), 731)
+    assertEquals(result, Right(731))
   }
 
   test("eval 2") {
-    val input = "10 - 2 - 3"
-    val lexer = ArithmeticLexer.lexer(input)
-    val result = parser(lexer)
+    val result = strParser("10 - 2 - 3")
 
-    assert(result.isRight)
-    assertEquals(result.getOrElse(0), 5)
+    assertEquals(result, Right(5))
   }
 
-  test("error 1") {
+  test("syntax error") {
     val input = "10 - * 2 - 3"
-    val lexer = ArithmeticLexer.lexer(input)
-    val result = parser(lexer)
+    val result = strParser("10 - * 2 - 3")
 
-    assert(result.isLeft)
+    result match
+      case Left(err) => assert(err.isInstanceOf[ErrorMsg.SyntaxError[_]])
+      case _ => fail(s"Input '$input' should fail (syntax error)")
   }
 
-  test("error 2") {
+  test("lex error") {
     val input = "10 _ 8"
-    val lexer = ArithmeticLexer.lexer(input)
-    val result = parser(lexer)
+    val result = strParser(input)
 
-    assert(result.isLeft)
-    assertEquals(result.swap.toOption.get, ErrorMsg.LexError(Token.ERROR("_")))
+    result match
+      case Left(err) => assertEquals(err, ErrorMsg.LexError(Token.ERROR("_")))
+      case _ => fail(s"Input '$input' should fail (lex error)")
   }
+
+  test("ended unexpectedly error") {
+    val input = "10 + ( 3"
+    val result = strParser(input)
+
+    result match
+      case Left(err) => assert(err.isInstanceOf[ErrorMsg.InputEndedUnexpectedly])
+      case _ => fail(s"Input '$input' should fail (input ended unexpectedly error)")
+  }
+
+  test("empty string") {
+    val input = ""
+    val result = strParser(input)
+    /* pudu.lexer.Lexer always generates an EOF token, so the error in this case
+     * is an InputEndedUnexpectedly error */
+    result match
+      case Left(err) => assert(err.isInstanceOf[ErrorMsg.InputEndedUnexpectedly])
+      case _ => fail(s"Input '$input' should fail (input ended unexpectedly error)")
+  }
+
 }
